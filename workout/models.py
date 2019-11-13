51cd8edb7dfe3c00d767688cec29de7103b1dc64
Cooper Tuch <coopertuch@gmail.com>
2019-11-13 11:57:43 -0800

    WeWorkout

--- workout/models.py
+++ workout/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Workout(models.Model):
-    name = models.CharField(max_length=100)
     muscle = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.muscle
 
 class Lift(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE, related_name='lifts')
