a345bb63e671e65a284f1541e53566d382921773
Cooper Tuch <coopertuch@gmail.com>
2019-11-15 10:17:21 -0800

    css

--- workout_django/settings.py
+++ workout_django/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['we-work-out.herokuapp.com']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'workout_django.urls'
